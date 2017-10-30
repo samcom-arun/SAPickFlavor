e2fe75230c41547321f71a31d45d69efe0cacf1b
samirkhatri <samcom@MAC3-3.local>
2017-10-30 17:05:06 +0530

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'SAPickFlavor' do
+  pod 'Alamofire'
+  pod 'MBProgressHUD', '~> 0.9.0'
+end
---
+++ SAPickFlavor/SAPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  SAPickFlavor.h
+//  SAPickFlavor
+//
+//  Created by MAC3 on 10/30/17.
+//  Copyright © 2017 app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SAPickFlavor.
+FOUNDATION_EXPORT double SAPickFlavorVersionNumber;
+
+//! Project version string for SAPickFlavor.
+FOUNDATION_EXPORT const unsigned char SAPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SAPickFlavor/PublicHeader.h>
+
+
